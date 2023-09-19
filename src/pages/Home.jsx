302eb7ece499e54a865ea397aeacfb2229c736da
ochosteve <ochosteve08@gmail.com>
2023-09-19 12:24:19 +0100

    add image card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,31 @@
 import ImageCard from "../Components/ImageCard";
 import GalleryList from "../Data";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 const Home = () => {
   const [images, setImages] = useState(GalleryList);
+
+  const moveImage = useCallback((dragIndex, hoverIndex) => {
+    setImages((prevCards) => {
+      const clonedCards = [...prevCards];
+      const removedItem = clonedCards.splice(dragIndex, 1)[0];
+
+      clonedCards.splice(hoverIndex, 0, removedItem);
+      return clonedCards;
+    });
+  }, []);
+
   console.log(images);
   return (
     <main>
-      {images.map((image) => (
+      {images.map((image, index) => (
         <ImageCard
           key={image.id}
           src={image.img}
           title={image.title}
           id={image.id}
+          index={index}
+          moveImage={moveImage}
         />
       ))}
     </main>
