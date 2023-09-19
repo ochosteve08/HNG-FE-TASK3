5731514197a3b8f926d1509da2bb52233dcc08e0
ochosteve <ochosteve08@gmail.com>
2023-09-19 13:28:21 +0100

    add tags to images in database

--- src/Data.jsx
+++ src/Data.jsx
@@ -14,53 +14,65 @@ const GalleryList = [
     id: 1,
     title: "Family",
     img: family,
-    tags: ["nature", "landscape"],
+    tags: ["people", "togetherness", "happiness"],
   },
   {
     id: 2,
     title: "Finance",
     img: finance,
+    tags: ["money", "business", "economy"],
   },
   {
     id: 3,
     title: "Bedroom",
     img: bedroom,
+    tags: ["furniture", "room", "comfort"],
   },
   {
     id: 4,
     title: "Mortgage",
     img: mortgage,
+    tags: ["house", "loan", "finance"],
   },
   {
     id: 5,
     title: "Meeting",
     img: meeting,
+    tags: ["business", "discussion", "conference"],
   },
   {
     id: 6,
     title: "Lottery",
     img: lottery,
+    tags: ["luck", "game", "chance"],
   },
   {
     id: 7,
     title: "Car",
     img: car,
+    tags: ["vehicle", "transport", "automobile"],
   },
   {
     id: 8,
     title: "Kitchen",
     img: kitchen,
+    tags: ["food", "cooking", "room"],
   },
   {
     id: 9,
     title: "Study Room",
     img: study,
+    tags: ["education", "books", "room"],
   },
   {
     id: 10,
     title: "Solar Panel",
     img: solar,
+    tags: ["energy", "renewable", "technology"],
   },
 ];
+
+
+
 
 export default GalleryList;
