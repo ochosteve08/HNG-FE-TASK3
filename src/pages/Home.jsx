25869e7b58f6a470f66a466736107f3fc6c9e108
ochosteve <ochosteve08@gmail.com>
2023-09-19 13:25:03 +0100

    add imageSearch component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import ImageCard from "../Components/ImageCard";
 import GalleryList from "../Data";
 import { useState, useCallback } from "react";
+import ImageSearch from "../Components/ImageSearch";
 
 const Home = () => {
   const [images, setImages] = useState(GalleryList);
+  const [filteredImages, setFilteredImages] = useState(GalleryList);
 
   const moveImage = useCallback((dragIndex, hoverIndex) => {
     setImages((prevCards) => {
@@ -15,10 +17,23 @@ const Home = () => {
     });
   }, []);
 
+  const handleSearch = (searchTerm) => {
+    if (searchTerm) {
+      const term = searchTerm.toLowerCase();
+      const filtered = images.filter((image) =>
+        image?.tags.some((tag) => tag.toLowerCase().includes(term))
+      );
+      setFilteredImages(filtered);
+    } else {
+      setFilteredImages(images);
+    }
+  };
+
   console.log(images);
   return (
     <main>
-      {images.map((image, index) => (
+      <ImageSearch onSearch={handleSearch} />
+      {filteredImages.map((image, index) => (
         <ImageCard
           key={image.id}
           src={image.img}
